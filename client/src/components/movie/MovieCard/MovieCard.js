6270760f12aed1d2445a27713ff4c28706c7305a
VadimGodovikov <vadimka.332@mail.ru>
2024-03-29 20:18:42 +0300

    Доработка карточек фильма

--- client/src/components/movie/MovieCard/MovieCard.js
+++ client/src/components/movie/MovieCard/MovieCard.js
@@ -2,11 +2,11 @@ import React from "react";
 import './MovieCard-CSS.css';
 
 const MovieCard = ({ movie }) => {
-    console.log(movie);
     return (
         <div className="movie-card">
-            <img class="img-moviecard" src={movie.poster.url} alt={movie.name || movie.altenativeName}></img>
-            <h2 class="name-moviecard">{movie.name || movie.altenativeName}</h2>
+            <img class="img-movie" src={movie.poster.url} alt={movie.name || movie.altenativeName}></img>
+            <h3 class="name-movie">{movie.name || movie.altenativeName}</h3>
+            <h4 class="options-movie">{movie.year},  Рейтинг:&#8201;&#8201; {movie.rating.kp} / 10</h4>
         </div>
     );
 };
--- client/src/components/movie/MovieSlider/MovieSlider.js
+++ client/src/components/movie/MovieSlider/MovieSlider.js
@@ -10,9 +10,9 @@ const MovieSlider = ({ genre }) => {
     useEffect(() => {
         const fetchMovies = async () => {
           try {
-            const moviesData = await axios.get(`https://api.kinopoisk.dev/v1.4/movie?genres.name=${genre}`, {
+            const moviesData = await axios.get(`https://api.kinopoisk.dev/v1.4/movie?genres.name=${genre}&year=2024`, {
               headers: {
-                'X-API-KEY': '6BPY2WX-2RMM7RA-NZW601H-8CC689V'
+                'X-API-KEY': 'CWGJ2C8-VGZM3FC-N2T2ENY-1S0QMWS'
               }
             });
             setMovies(moviesData.data.docs);
@@ -25,18 +25,18 @@ const MovieSlider = ({ genre }) => {
       }, [genre]);
 
       if(!movies) {
-        return (<h1>Загрузка</h1>);
+        return (<h1>Загрузка...</h1>);
       }
       else{
         return (
           <div className="movie-slider">
             <h2 className="genre-title">{genre}</h2>
-            <Carousel interval={5000} indicators={false} style={{ height: 300 }}>
+            <Carousel interval={5000} indicators={false}>
               {[...Array(Math.ceil(movies.length / 5))].map((_, index) => (
                 <CarouselItem key={index}>
-                  <Stack direction="horizontal" className="h-100 justify-content-center align-items-center" gap={3}>
+                  <Stack direction="horizontal" className="h-100 justify-content-center align-items-down" gap={5}>
                     {movies.slice(index * 5, (index + 1) * 5).map(movie => (
-                      <Card key={movie.id}>
+                      <Card key={movie.id} style={{ border: 0 }}>
                         <MovieCard movie={movie}/>
                       </Card>
                     ))}
